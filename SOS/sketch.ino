f798253da597f95529160d624988a19b2f64ffd5
Gaful <114806464+GafulOne@users.noreply.github.com>
2024-02-12 16:06:42 +0300

    Add files via upload

---
+++ SOS/sketch.ino
@@ -0,0 +1,22 @@
+void setup() 
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(2 * 500);
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
+}
